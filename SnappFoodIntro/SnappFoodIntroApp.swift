2cb5d18e06bcef087224877c5c07c519aa7485e3
mohsen mokhtari <mr.marsxan@gmail.com>
2023-08-12 12:36:09 +0330

    Initial Commit

---
+++ SnappFoodIntro/SnappFoodIntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnappFoodIntroApp.swift
+//  SnappFoodIntro
+//
+//  Created by mohsen mokhtari on 8/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SnappFoodIntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
