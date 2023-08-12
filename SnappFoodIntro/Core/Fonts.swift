5e887d7babeaefaad77106529ac7870af323e686
mohsen mokhtari <mr.marsxan@gmail.com>
2023-08-12 17:44:59 +0330

    init supporting files and images

---
+++ SnappFoodIntro/Core/Fonts.swift
@@ -0,0 +1,12 @@
+//
+//  Fonts.swift
+//  SnappFoodIntro
+//
+//  Created by mohsen mokhtari on 8/12/23.
+//
+
+import Foundation
+
+let iranSansBold = "IRANSansWeb-Bold"
+let iranSansMedium = "IRANSansWeb-Medium"
+let iranSansRegular = "IRANSansWeb"
---
+++ SnappFoodIntro/Core/Images.swift
@@ -0,0 +1,11 @@
+//
+//  Images.swift
+//  SnappFoodIntro
+//
+//  Created by mohsen mokhtari on 8/12/23.
+//
+
+struct Images{
+    static let foodImageList = ["Food_1","Food_2","Food_3","Food_4","Food_3","Food_1"]
+    static let logo = "FoodLogo"
+}
